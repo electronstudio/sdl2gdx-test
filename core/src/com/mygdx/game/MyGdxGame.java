e15880508333d66f059d6d939a447607c20fd625
electronstudio <misc@fantastic.me.uk>
2020-03-26 22:44:07 +0000

    update to work with latest sdl2gdx that is stricter about what apis it exposes

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.libsdl.SDL;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float x, y;
 	
 	@Override
 	public void create () {
@@ -23,10 +25,16 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
+		for (Controller controller : Controllers.getControllers()) {
+			x += controller.getAxis(SDL.SDL_CONTROLLER_AXIS_LEFTX);
+			y -= controller.getAxis(SDL.SDL_CONTROLLER_AXIS_LEFTY);
+		}
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
